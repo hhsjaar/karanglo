c098f376536010e690eebfee98812b923c7082c3
hhsjaar <hhsjaar@gmail.com>
2026-02-04 03:46:03 +0700

    2

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
         seed: "ts-node --compiler-options {\"module\":\"CommonJS\"} prisma/seed.ts"
     },
     datasource: {
-        url: process.env["DATABASE_URL"],
-        directUrl: process.env["DIRECT_URL"],
+        url: process.env["DIRECT_URL"] || process.env["DATABASE_URL"],
     },
 });
